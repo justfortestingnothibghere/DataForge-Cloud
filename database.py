0a6ee8f08f4d5185c7ffb6fe0b02e362f3eb177b
justfortestingnothibghere <armanhacker900@gmail.com>
2025-11-03 03:24:40 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 
 # Env vars
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://localhost/dataforge")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://todo_99ka_user:tPR9uvBxr27EADVUolBJ10emUPK8FMxh@dpg-d3qmgf0gjchc73bfnnng-a/todo_99ka")
 
 # Database setup
 engine = create_engine(DATABASE_URL)
