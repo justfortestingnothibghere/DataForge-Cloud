7f0520c68b8841cbca77485de539e07b2531fcbc
justfortestingnothibghere <armanhacker900@gmail.com>
2025-11-03 02:22:32 +0530

    Update admin.py

--- routes/admin.py
+++ routes/admin.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 import os
 import glob
-from app import get_current_user
+from utils import get_current_user
 from database import get_db
 from models import User, Upload
 
